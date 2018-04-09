13f2cd5375bbeced7b8da806257116b551b8e3c6
DavidKerns <davidkerns@yahoo.com>
2018-04-09 19:50:25 -0400

    added packages

---
+++ app.js
@@ -0,0 +1,174 @@
+const express            = require('express');
+const path               = require('path');
+const favicon            = require('serve-favicon');
+const logger             = require('morgan');
+const cookieParser       = require('cookie-parser');
+const bodyParser         = require('body-parser');
+const expressLayouts     = require('express-ejs-layouts');
+const mongoose           = require('mongoose');
+const passport           = require('passport');
+const session            = require('express-session');
+const MongoStore         = require('connect-mongo')(session);
+const LocalStrategy      = require('passport-local').Strategy;
+const bcrypt             = require('bcrypt');
+const multer             = require('multer');
+// const Subscription       = require('./models/subscription');
+const User               = require('./models/user');
+const Product            = require('./models/products');
+const Subscription       = require('./models/subscription');
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI);
+const index = require('./routes/index');
+const authRoutes = require('./routes/authentication');
+const productRoutes = require('./routes/products');
+const subRoutes = require('./routes/subscription');
+
+const app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main-layout');
+app.use(expressLayouts);
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use( (req, res, next) => {
+  if (typeof(req.user) !== "undefined"){
+    res.locals.userSignedIn = true;
+  } else {
+    res.locals.userSignedIn = false;
+  }
+  next();
+});
+
+app.use(session({
+  secret: 'ironChef',
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection })
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use('local-signup', new LocalStrategy(
+  { passReqToCallback: true },
+  (req, username, password, next) => {
+    console.log("req.body````````",req.body);
+    process.nextTick(() => {
+        User.findOne({
+            'username': username
+        }, (err, user) => {
+            if (err){ return next(err); }
+
+            if (user) {
+                return next(null, false);
+            } else {
+                // Destructure the body
+                const { username, email, position, address, password } = req.body;
+                const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+                const newUser = new User({
+	                 // required stuff: email and password
+	                  username: req.body.username,
+	                  password: hashPass,
+	                   // anything extra goes in here too
+                      address : req.body.address,
+	                    email   : req.body.email,
+	                    position: req.body.position,
+                      interest: req.body.interest,
+
+                    });
+
+newUser.save((err) => {
+	if (err){ next(err); }
+	return next(null, newUser);
+});
+
+                newUser.save((err) => {
+                    if (err){ next(err); }
+                    return next(null, newUser);
+                });
+            }
+        });
+    });
+}));
+
+passport.use('local-login', new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use( (req, res, next) => {
+  if (req.user) {
+    res.locals.user = req.user;
+  }
+  next();
+});
+
+app.use('/', index);
+app.use(authRoutes);
+app.use('/products', productRoutes);
+app.use('/subscription', subRoutes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
